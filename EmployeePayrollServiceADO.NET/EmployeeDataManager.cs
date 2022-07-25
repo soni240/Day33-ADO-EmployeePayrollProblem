8ac6fd51d47638903a829c8f320e6ac37bcae9f3
soni240 <s.fegade21@gmail.com>
2022-07-25 14:51:37 +0530

    [Gunavanti]Add:create database

---
+++ EmployeePayrollServiceADO.NET/EmployeeDataManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollServiceADO.NET
+{
+    public class EmployeeDataManager
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double salary { get; set; }
+        public DateTime startDate { get; set; }
+        public char Gender { get; set; }
+        public Int64 EmployeePhoneNumber { get; set; }
+        public string EmployeeDepartment { get; set; }
+        public string Address { get; set; }
+        public double Deduction { get; set; }
+        public double IncomeTax { get; set; }
+        public double TaxablePay { get; set; }
+        public double NetPay { get; set; }
+    }
+}
---
+++ EmployeePayrollServiceADO.NET/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace EmployeePayRollServiceADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------Welcome TO Employee PayRoll Services Using ADO.NET");
+
+        }
+    }
+}
