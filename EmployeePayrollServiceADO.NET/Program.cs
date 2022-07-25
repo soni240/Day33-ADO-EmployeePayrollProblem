2401f703d4856ff5aeabaa6b37e0139265f6384b
soni240 <s.fegade21@gmail.com>
2022-07-25 14:56:45 +0530

    [gunvanti]Add:retrieve data from database

--- EmployeePayrollServiceADO.NET/Program.cs
+++ EmployeePayrollServiceADO.NET/Program.cs
@@ -1,14 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using EmployeePayrollServiceADO.NET;
 using System;
 
-namespace EmployeePayRollServiceADO.NET
+namespace EmployeePayrollServiceADO.NET
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("------------Welcome TO Employee PayRoll Services Using ADO.NET");
+            Console.WriteLine("------------Welcome TO Employee PayRoll Services Using ADO.NET----------");
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+            employeeRepository.GetDataFromSql();
 
         }
     }
